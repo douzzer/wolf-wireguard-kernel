9873d99e1eb363b824c3bf8f7530d84639e34491
Daniel Pouzzner <douzzer@wolfssl.com>
2025-09-05 16:42:32 -0500

    src/socket.c: fix for kernel 6.17.

--- src/socket.c
+++ src/socket.c
@@ -86,9 +86,15 @@ static int send4(struct wg_device *wg, struct sk_buff *skb,
 	}
 
 	skb->ignore_df = 1;
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 17, 0)
 	udp_tunnel_xmit_skb(rt, sock, skb, fl.saddr, fl.daddr, ds,
 			    ip4_dst_hoplimit(&rt->dst), 0, fl.fl4_sport,
 			    fl.fl4_dport, false, false);
+#else
+	udp_tunnel_xmit_skb(rt, sock, skb, fl.saddr, fl.daddr, ds,
+			    ip4_dst_hoplimit(&rt->dst), 0, fl.fl4_sport,
+			    fl.fl4_dport, false, false, 0);
+#endif
 	goto out;
 
 err:
@@ -157,9 +163,15 @@ static int send6(struct wg_device *wg, struct sk_buff *skb,
 	}
 
 	skb->ignore_df = 1;
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 17, 0)
 	udp_tunnel6_xmit_skb(dst, sock, skb, skb->dev, &fl.saddr, &fl.daddr, ds,
 			     ip6_dst_hoplimit(dst), 0, fl.fl6_sport,
 			     fl.fl6_dport, false);
+#else
+	udp_tunnel6_xmit_skb(dst, sock, skb, skb->dev, &fl.saddr, &fl.daddr, ds,
+			     ip6_dst_hoplimit(dst), 0, fl.fl6_sport,
+			     fl.fl6_dport, false, 0);
+#endif
 	goto out;
 
 err:
